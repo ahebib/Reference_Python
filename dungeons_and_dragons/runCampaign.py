4bbb8fa5cce7f30c5f3a4b84b53d6e83612cd4f6
Ammar Hebib <ammar.hebib@gmail.com>
2021-05-01 21:13:17 -0400

    Add input and response

--- dungeons_and_dragons/runCampaign.py
+++ dungeons_and_dragons/runCampaign.py
@@ -15,7 +15,18 @@ def loadCharacter():
     return characterJSON
 
 def startCampaign(campaignCharacter):
-    print('So the journey begins for...')
-    print(campaignCharacter.getName() + ' the ' + campaignCharacter.getRace() + ' ' + campaignCharacter.getClass())
+    print('So the journey begins for ' + campaignCharacter.getName() + ' the ' + campaignCharacter.getRace() + ' ' + campaignCharacter.getClass() + '...')
+    print('\nloading...(not really)\n')
+
+    response = askPlayerResponse('You wake up in your bed, groggy, and feeling like you could get some sleep but you have to use the make kill and make your breakfast and make yourself a morning drink. \nWhere do you want to start(breakfast, drink)? ')
+
+    if (response == 'breakfast'):
+        print('Go get breakfast')
+    elif (response == 'drink'):
+        print('Go make your drink')
+    
+
+def askPlayerResponse(q):
+    return input(q)
 
 main()
